3814315799ce151b3ba663b95a8b7b43565346b7
kykp <kykp@yandex.ru>
2022-10-27 14:51:12 +0400

    heroky try

--- mongoconnect.ts
+++ mongoconnect.ts
@@ -1,12 +1,11 @@
 import mongoose from "mongoose";
 import * as dotenv from 'dotenv'  
 dotenv.config()
-const mongoPassword = process.env.MONGO_PASS;
-const mongoLogin = process.env.MONGO_LOGIN;
+const mongoUrl =  process.env.MONGO__URI;
 
 async function startBaseMongo() {
   const uri =
-    `mongodb+srv://${mongoLogin}:${mongoPassword}@cluster0.xtwcw.mongodb.net/Users?retryWrites=true&w=majority`;
+    `${mongoUrl}`;
   try {
     console.log("успешное подключение к БД");
     mongoose.connect(uri);
