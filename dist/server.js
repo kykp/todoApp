3a94166cdfaf099ced6dc5a5f474d6a1245a57dc
kykp <kykp@yandex.ru>
2022-10-27 11:11:22 +0400

    try upload to heroku

--- dist/server.js
+++ dist/server.js
@@ -8,6 +8,7 @@ import cors from "cors";
 const PORT = process.env.PORT || 5000;
 const app = express();
 app.use(cors());
+app.use(express.static('dist'));
 app.use(express.json());
 app.use("/projects", urlencodedParser, projectRouter);
 app.use("/tasks", urlencodedParser, taskRouter);
